38f8952f837e2e967d7cf2cd36579c98fe292ba8
Hyeona Son <89680808+HyeonaSon@users.noreply.github.com>
2026-05-05 14:37:54 +0900

    config.js

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://xguqyniswqdvzfqauwkg.supabase.co';  // 본인 값으로 교체
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhndXF5bmlzd3FkdnpmcWF1d2tnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NDczMTEsImV4cCI6MjA5MzUyMzMxMX0.aINcMUcNhavawrZgWTsdyXzTdxSZohVxRLxEQTvJ5Ag';                    // 본인 값으로 교체
